dd40ae05bc767907d88cb01462b527c1f204c63d
AidenColin024 <1212500@student.roc-nijmegen.nl>
2026-06-25 12:21:44 +0200

    Aanmaken gebruiker class

---
+++ Gebruiker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spativy
+{
+    internal class Gebruiker
+    {
+        public String NaamGebruiker { get; set; }
+        public List<Nummer> Nummers { get; set; }
+        public List<Album> Albums { get; set; }
+        public List<Afspeelijst> Afspeellijsten { get; set; }
+    public Gebruiker(String naamgebruiker, List<Nummer> nummers, List<Album> albums, List<Afspeelijst> afspeellijsten)
+        {
+            NaamGebruiker = naamgebruiker;
+            Nummers = nummers;
+            Albums = albums;
+            Afspeellijsten = afspeellijsten;
+
+
+        }
+    }
+}
